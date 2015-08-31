de978ea3311786a736b175482e2dbdbc419aa3d3
Timothy Stott <stott.timothy@gmail.com>
2015-08-31 14:11:36 +0100

    m00 Everything in order?

--- bank.js
+++ bank.js
@@ -1,23 +1,11 @@
+var helloWorld = function() {
+  console.log('Hello World ' + new Date());
+}
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  helloWorld();
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  helloWorld();
 }
