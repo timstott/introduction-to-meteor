fe17245a634c01733cf8d26aac08295307dc149b
Timothy Stott <stott.timothy@gmail.com>
2015-08-31 21:15:08 +0100

    m02 Meteor methods

--- bank.js
+++ bank.js
@@ -5,18 +5,48 @@ var helloWorld = function() {
 // NOTE: The Transactions collection is accessible on the client and server
 Transactions = new Mongo.Collection('transactions')
 
+Meteor.methods({
+  'transactions/duplicate': function (transactionId) {
+    check(transactionId, String);
+    transaction = Transactions.findOne(transactionId);
+    if (!transaction) {
+      throw new Meteor.Error('document-not-found', 'Document not found.')
+    } else {
+      Transactions.update({ _id: transactionId },
+                          { $set: { duplicate: !transaction.duplicate } })
+    }
+  }
+});
+
 if (Meteor.isClient) {
   helloWorld();
 
   Template.transactions.helpers({
     transactions: function () {
       return Transactions.find();
+    },
+    isDuplicate: function() {
+      return this.duplicate ? 'duplicate' : '';
     }
   });
 
   Template.registerHelper('formatTime', function(context, options) {
     return moment(context).format('MM/DD/YYYY HH:mm');
   });
+
+  Template.transactions.events({
+    'click a.duplicate': function (event) {
+      // NOTE: Helpers receive the current template context data in `this`
+      var transactionId = this._id;
+
+      // NOTE: Meteor method is executed on the client and server (latency compensation).
+      Meteor.call('transactions/duplicate', transactionId, function(err) {
+        if (err)
+          alert(error.message)
+      });
+      event.preventDefault();
+    }
+  })
 }
 
 if (Meteor.isServer) {
